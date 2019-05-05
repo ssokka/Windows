3850fa08ab667bd135c4e272ee05ad79305e7827
ssokka <ssokka@knou.ac.kr>
2019-05-05 22:58:42 +0900

    2019.05.05

--- mvcr/run.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-powershell.exe -command "(new-object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/ssokka/windows/master/tools/nircmdc.exe\",\"nircmdc.exe\")"
-powershell.exe -command "(new-object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/ssokka/windows/master/mvcr/install.cmd\",\"install.cmd\")"
-powershell.exe -command "(Get-Content \"install.cmd\" -Raw).Replace("`n","`r`n") | Set-Content \"install.cmd\" -Force"
-nircmdc.exe elevate install.cmd
