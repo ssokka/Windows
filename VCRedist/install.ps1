032d20afb2ebe4fd5f630ae98b0a595c3f198943
ssokka <ssokka@knou.ac.kr>
2019-05-07 03:50:03 +0900

    2019.05.07

--- VCRedist/install.ps1
+++ VCRedist/install.ps1
@@ -30,22 +30,19 @@ $data = Invoke-RestMethod 'https://raw.githubusercontent.com/ssokka/Windows/mast
 		if ($item.Product -eq 2005) { $log = '' }
 		$CLO = '{0} {1}' -f $item.CommandLineOptions, $log
 		$url = if ($OSArch -eq 86) { $item.x86 } else { $item.x64 }
-		Write-Host "   $name" -NoNewline
-		Write-Host " | 다운로드 " -NoNewline
+		Write-Host "`r   $name | 다운로드 중..." -NoNewline
 		(New-Object System.Net.WebClient).DownloadFile("$url", "$file")
-		if (Test-Path -Path "$file") {
-			Write-Host "완료" -NoNewline
-		} else {
-			Write-Host "실패" -ForegroundColor Red
+		if (-not (Test-Path -Path "$file")) {
+			Write-Host "`r   $name | 다운로드 실패" -ForegroundColor Red
 			continue osarch
 		}
-		Write-Host " | 설치 " -NoNewline
+		Write-Host "`r   $name | 설치 중..." -NoNewline
 		$process = Start-Process -FilePath "$file" -ArgumentList $CLO -PassThru -Verb RunAs -Wait
-		if ($process.ExitCode -eq 3010) { $restart = $true }
-		if ($process.ExitCode -ne 0 -and $process.ExitCode -ne 3010) {
-			Write-Host "실패" -ForegroundColor Red
+		if ($process.ExitCode -eq 0 -or $process.ExitCode -eq 3010) {
+			if ($process.ExitCode -eq 3010) { $restart = $true }
+			Write-Host "`r   $name | 설치 완료"
 		} else {
-			Write-Host "완료"
+			Write-Host "`r   $name | 설치 실패" -ForegroundColor Red
 		}
 	}
 }
