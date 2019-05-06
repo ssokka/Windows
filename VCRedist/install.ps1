416e36317837ad3e2d47fb8aaf53e13dd2a687d4
ssokka <ssokka@knou.ac.kr>
2019-05-06 22:04:29 +0900

    2019.05.06

--- VCRedist/install.ps1
+++ VCRedist/install.ps1
@@ -1,6 +1,7 @@
 # ANSI / CP949 / CRLF
 
 $title = 'Microsoft Visual C++ 재배포 가능 패키지 설치'
+$host.ui.RawUI.WindowTitle = $title
 Write-Host "`r`n### $title 시작 ###`r`n"
 
 $OSBit = if ([IntPtr]::Size -eq 4) {32} else {64}
@@ -27,7 +28,7 @@ foreach ($item in $data) {
 		if ($item.Product -eq 2005) {$log = ''}
 		$CLO = '{0} {1}' -f $item.CommandLineOptions, $log
 		$url = if ($OSArchs -eq 86) {$item.x86} else {$item.x64}
-		Write-Host "    + $name 다운로드" -NoNewline
+		Write-Host "$name | 다운로드" -NoNewline
 		(New-Object System.Net.WebClient).DownloadFile("$url", "$file")
 		if (Test-Path -Path "$file") {
 			Write-Host " 완료" -NoNewline
@@ -35,7 +36,7 @@ foreach ($item in $data) {
 			Write-Host " 실패 !" -ForegroundColor Red
 			continue
 		}
-		Write-Host " 설치" -NoNewline
+		Write-Host " | 설치" -NoNewline
 		$process = Start-Process -FilePath "$file" -ArgumentList $CLO -PassThru -Verb RunAs -Wait
 		if ($process.ExitCode -eq 3010) {$restart = $true}
 		if ($process.ExitCode -ne 0 -and $process.ExitCode -ne 3010) {Write-Host " 실패 !" -ForegroundColor Red}
@@ -46,7 +47,8 @@ foreach ($item in $data) {
 Write-Host "### $title 완료 ###`r`n"
 
 if ($restart) {
-	Write-Host "설치를 완료하려면 컴퓨터를 다시 시작해야 합니다.`r`n지금 컴퓨터를 다시 시작하시겠습니까? [Y/N]" -NoNewline -ForegroundColor Red
+	Write-Host "설치를 완료하려면 컴퓨터를 다시 시작해야 합니다."
+	Write-Host "지금 컴퓨터를 다시 시작하시겠습니까? [Y/N] " -NoNewline -ForegroundColor Red
 	$input = Read-Host
 	if ($input -eq "y") {Restart-Computer -Force}
 } else {
