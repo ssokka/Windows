2fabdee6fb3fc8f1d66f90ae34b4371f83dc0980
ssokka <ssokka@knou.ac.kr>
2019-05-06 23:01:42 +0900

    2019.05.06

--- VCRedist/install.ps1
+++ VCRedist/install.ps1
@@ -2,7 +2,7 @@
 
 $title = 'Microsoft Visual C++ 재배포 가능 패키지 설치'
 $host.ui.RawUI.WindowTitle = $title
-Write-Host "`r`n### $title 시작 ###`r`n"
+Write-Host "`r`n # $title 시작 #`r`n" -ForegroundColor Blue
 
 $OSBit = if ([IntPtr]::Size -eq 4) {32} else {64}
 $OSArchs = 86, 64
@@ -28,7 +28,7 @@ foreach ($item in $data) {
 		if ($item.Product -eq 2005) {$log = ''}
 		$CLO = '{0} {1}' -f $item.CommandLineOptions, $log
 		$url = if ($OSArchs -eq 86) {$item.x86} else {$item.x64}
-		Write-Host "$name | 다운로드" -NoNewline
+		Write-Host "   $name | 다운로드" -NoNewline
 		(New-Object System.Net.WebClient).DownloadFile("$url", "$file")
 		if (Test-Path -Path "$file") {
 			Write-Host " 완료" -NoNewline
@@ -39,19 +39,23 @@ foreach ($item in $data) {
 		Write-Host " | 설치" -NoNewline
 		$process = Start-Process -FilePath "$file" -ArgumentList $CLO -PassThru -Verb RunAs -Wait
 		if ($process.ExitCode -eq 3010) {$restart = $true}
-		if ($process.ExitCode -ne 0 -and $process.ExitCode -ne 3010) {Write-Host " 실패 !" -ForegroundColor Red}
+		if ($process.ExitCode -ne 0 -and $process.ExitCode -ne 3010) {
+			Write-Host " 실패 !" -ForegroundColor Red
+		else {
+			Write-Host " 완료"
+		}
 	}
 	Write-Host "`r`n" 
 }
 
-Write-Host "### $title 완료 ###`r`n"
+Write-Host " # $title 완료 #`r`n" -ForegroundColor Blue
 
 if ($restart) {
-	Write-Host "설치를 완료하려면 컴퓨터를 다시 시작해야 합니다."
-	Write-Host "지금 컴퓨터를 다시 시작하시겠습니까? [Y/N] " -NoNewline -ForegroundColor Red
+	Write-Host " 설치를 완료하려면 컴퓨터를 다시 시작해야 합니다."
+	Write-Host " 지금 컴퓨터를 다시 시작하시겠습니까? [Y/N] " -NoNewline -ForegroundColor Red
 	$input = Read-Host
 	if ($input -eq "y") {Restart-Computer -Force}
 } else {
-	Write-Host "아무키나 누르십시오." -NoNewline
+	Write-Host " 아무키나 누르십시오." -NoNewline
 	Read-Host
 }
