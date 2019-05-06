eb4c6e5a377fe896218230ce2011023ca109ce23
ssokka <ssokka@knou.ac.kr>
2019-05-06 21:26:58 +0900

    2019.05.06

--- VCRedist/ps2exe.ps1
+++ VCRedist/ps2exe.ps1
@@ -1,4 +1,3 @@
 # UTF-8 / CRLF
 
-$url = 'https://raw.githubusercontent.com/ssokka/Windows/master/VCRedist/install.ps1'
-(New-Object System.Net.WebClient).DownloadString($url)
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/VCRedist/install.ps1'))
