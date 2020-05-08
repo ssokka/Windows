e88b410de94141e74d09d638295ec828724d8222
ssokka <ssokka@knou.ac.kr>
2020-05-08 11:16:11 +0900

    2020.05.08

---
+++ Cmd/D2Coding.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+pushd %temp%
+
+set _ps=powershell.exe -nop -ep bypass
+set _url=https://raw.githubusercontent.com/ssokka/Windows/master/PowerShell
+set _ps1=font.ps1
+
+%_ps% -c "& {[Net.WebClient]::new().DownloadFile('%_url%/%_ps1%', '%_ps1%')}"
+
+if exist "%_ps1%" %_ps% -f "%_ps1%"
