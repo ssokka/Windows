9880a79f87bf5db687a636183b7eb7b418cbbf46
ssokka <ssokka@knou.ac.kr>
2020-05-11 12:24:41 +0900

    2020.05.11

---
+++ CommandPrompt/D2Coding.cmd
@@ -0,0 +1,12 @@
+@echo off
+:: windows utf-8 crlf
+setlocal
+pushd %temp%
+
+set _ps1=font.ps1
+set _url=https://raw.githubusercontent.com/ssokka/Windows/master/PowerShell
+set _ps=powershell.exe -nop -ep bypass
+
+%_ps% -c "& {[Net.WebClient]::new().DownloadFile('%_url%/%_ps1%', '%_ps1%')}"
+
+if exist "%_ps1%" %_ps% -f "%_ps1%" -p
--- PowerShell/font.ps1
+++ PowerShell/font.ps1
@@ -5,29 +5,46 @@ Param (
     [ValidateScript({@('.ttc','.ttf') -contains [IO.Path]::GetExtension($_)})]
     [string] $file = 'D2Coding.ttc',
     [string] $url = "https://raw.githubusercontent.com/ssokka/Fonts/master/$file",
-    # https://drive.google.com/uc?export=download&id=fileid
-    # [string] $url = 'https://drive.google.com/uc?export=download&id=fileid',
-    [switch] $pause = $true,
-    [switch] $debug,
-    [switch] $verbose
+    [switch] $p,            # pause then exit
+    [switch] $d,            # debug mode
+    [switch] $t             # test mode
 )
 
-# download functions.psm1
-[Net.WebClient]::new().DownloadFile('https://raw.githubusercontent.com/ssokka/Windows/master/PowerShell/functions.psm1', 'functions.psm1')
+# set messages
+$ErrorMessage = " ! 오류가 발생했습니다.`n"
+$ExitMessage = " * 스크립트를 종료합니다. 아무 키나 누르십시오.`n"
 
-# import functions.psm1
-Import-Module ([IO.Path]::Combine($PSScriptRoot, 'functions.psm1'))
-Get-Command -Module functions | Out-Null
+# download and import main.psm1
+try {
+    $module = 'module.psm1'
+    if ((!(Test-Path $module) -or $m) -and !$t) {
+        $temp = "${env:TEMP}\ssokka"
+        New-Item $temp -Type Directory -Force
+        Push-Location $temp
+        [Net.WebClient]::new().DownloadFile("https://raw.githubusercontent.com/ssokka/Windows/master/PowerShell/$module", "$temp\$module")
+    }
+    Import-Module "$temp\$module" -ErrorAction:Stop
+}
+catch {
+    Write-Error ($_.Exception | Format-List -Force | Out-String)
+    Write-Error ($_.InvocationInfo | Format-List -Force | Out-String)
+    Write-Host $ErrorMessage -ForegroundColor DarkRed
+    Write-Host $ExitMessage -NoNewline -ForegroundColor Gray; [void][Console]::ReadKey($true)
+    exit 1
+}
+
+# set window position size
+wps -n:$t
 
 # set title
 wt "$file 글꼴"
 
-# set directory
-$tff = [IO.Path]::Combine(${env:TEMP},$file)
-$sff = [IO.Path]::Combine(${env:SystemRoot},'Fonts',$file)
-$uff = [IO.Path]::Combine(${env:LOCALAPPDATA},'Microsoft','Windows','Fonts',$file)
+# set font files
+$tff = [IO.Path]::Combine(${env:TEMP}, $file)
+$sff = [IO.Path]::Combine(${env:SystemRoot}, 'Fonts', $file)
+$uff = [IO.Path]::Combine(${env:LOCALAPPDATA}, 'Microsoft', 'Windows', 'Fonts', $file)
 
-# set registry
+# set font registry keys
 $frk = 'Microsoft\Windows NT\CurrentVersion\Fonts'
 $srk = "HKLM:\SOFTWARE\$frk"
 $urk = "HKCU:\Software\$frk"
@@ -44,10 +61,12 @@ if (!(Test-Path $uff)) {
         e 1
     }
     $i = wh ' 설치' DarkGreen -r
-    sudo powershell.exe "(New-Object -ComObject Shell.Application).Namespace(0x14).CopyHere('$tff')"
+    adm powershell.exe "(New-Object -ComObject Shell.Application).Namespace(0x14).CopyHere('$tff')"
     if (!(Test-Path $uff)) {
-        $e = $space + "! 파일이 존재하지 않습니다.`n"
-        $e += $space + "! `$uff = $uff"
+        wh ' 실패' DarkRed -n
+        wh "! 파일이 존재하지 않습니다." -n
+        wh "! `$uff = $uff"
+        e 1
     }
 }
 
@@ -56,56 +75,50 @@ if (!$i) {
 }
 
 # check user registry font name
-if (!$e) {
-    $urn = (Get-ItemProperty $urk).PSObject.Properties | Where-Object Value -like *$file | Select-Object -ExpandProperty Name
-    if ($urn) {
-        # check system registry font name and file
-        if ((Get-ItemProperty $srk $urn -ErrorAction SilentlyContinue) -and (Test-Path $sff)) {
-            e 0
-        }
-    } else {
-        $e = $space + "! 글꼴 이름이 존재하지 않습니다.`n"
-        $e += $space + "! $tff"
+$urn = (Get-ItemProperty $urk).PSObject.Properties | Where-Object Value -like *$file | Select-Object -ExpandProperty Name
+if ($urn) {
+    # check system registry font name and file
+    if ((Get-ItemProperty $srk $urn -ErrorAction SilentlyContinue) -and (Test-Path $sff)) {
+        e 0
     }
+} else {
+    wh ' 실패' DarkRed -n
+    wh "! 글꼴 이름이 존재하지 않습니다." -n
+    wh "! $tff"
+    e 1
 }
 
 # copy user font file to system font file
-if (!$e) {
-    sudo powershell.exe "Copy-Item '$uff' '$sff' -Force"
-    if (!(Test-Path $sff)) {
-        $e = $space + "! 파일이 존재하지 않습니다.`n"
-        $e += $space + "! $sff"
-    }
+adm powershell.exe "Copy-Item '$uff' '$sff' -Force"
+if (!(Test-Path $sff)) {
+    wh ' 실패' DarkRed -n
+    wh "! 파일이 존재하지 않습니다." -n
+    wh "! $sff"
+    e 1
 }
 
 # remove user font regisrty and file
-if (!$e) {
-    sudo powershell.exe "Stop-Service FontCache"
-    Remove-ItemProperty $urk -Name $urn
-    Remove-Item $uff -Force
-    sudo powershell.exe "Start-Service FontCache"
-}
+adm powershell.exe "Stop-Service FontCache"
+Remove-ItemProperty $urk -Name $urn
+Remove-Item $uff -Force
+adm powershell.exe "Start-Service FontCache"
 
 # add system registry font name
-if (!$e) {
-    sudo powershell.exe "New-ItemProperty '$srk' '$urn' -PropertyType String -Value '$file'"
-    $srn = (Get-ItemProperty $srk).PSObject.Properties | Where-Object Value -like *$file | Select-Object -ExpandProperty Name
-    if (!$srn) {
-        $e = $space + "! 글꼴 이름이 존재하지 않습니다.`n"
-        $e += $space + "! $urn : $srk"
-    }
-    if (!$e -and $srn -ne $urn) {
-        sudo powershell.exe "Remove-ItemProperty '$srk' -Name '$srn'"
-        Remove-Item $sff -Force
-        $e = $space + "! 글꼴 이름이 다릅니다.`n"
-        $e += $space + "! $urn ≠ $srk"
-    }
+adm powershell.exe "New-ItemProperty '$srk' '$urn' -PropertyType String -Value '$file'"
+$srn = (Get-ItemProperty $srk).PSObject.Properties | Where-Object Value -like *$file | Select-Object -ExpandProperty Name
+if (!$srn) {
+    wh ' 실패' DarkRed -n
+    wh "! 글꼴 이름이 존재하지 않습니다." -n
+    wh "! $urn : $srk"
+    e 1
+}
+if (!$e -and $srn -ne $urn) {
+    adm powershell.exe "Remove-ItemProperty '$srk' -Name '$srn'"
+    Remove-Item $sff -Force
+    wh ' 실패' DarkRed -n
+    wh "! 글꼴 이름이 다릅니다." -n
+    wh "! $urn ≠ $srk"
+    e 1
 }
 
-if ($e) {
-    wh ($space + '실패' + $e) DarkRed -n
-    e 1
-} else {
-    wh -n
-    e 0
-}
+e 0
