3a08d860e351a51d63954a367c82de818e09b58e
ssokka <ssokka@knou.ac.kr>
2020-05-16 00:58:47 +0900

    2020.05.16

---
+++ Font/font.ps1
@@ -0,0 +1,137 @@
+# windows euc-kr crlf
+
+# parameters
+Param (
+    [ValidateScript({@(".ttc",".ttf") -contains [IO.Path]::GetExtension($_)})]
+    [string] $file = "D2Coding.ttc",
+    [string] $url,
+    [switch] $m, # force download module.psm1
+    [switch] $r, # remove working directory
+    [switch] $p, # pause then exit
+    [switch] $d, # debug mode
+    [switch] $t # test mode
+)
+
+$repository = "https://raw.githubusercontent.com/ssokka"
+
+# default font file url
+if (!$url) {
+    $url = "$repository/Fonts/master/$file"
+}
+
+# working directory
+$temp = "${env:TEMP}\ssokka"
+New-Item $temp -Type:Directory -Force | Out-Null
+
+# module download and import
+try {
+    $module = "module.psm1"
+    if ($t) {
+        Copy-Item $module "$temp\$module" -Force
+    } else {
+        if ((!(Test-Path $module) -or $m)) {
+            [Net.WebClient]::new().DownloadFile("$repository/Windows/master/PowerShell/$module", "$temp\$module")
+        }
+    }
+    Import-Module "$temp\$module" -ErrorAction:Stop
+}
+catch {
+    Write-Error ($_.Exception | Format-List -Force | Out-String)
+    Write-Error ($_.InvocationInfo | Format-List -Force | Out-String)
+    Write-Host " ! 오류가 발생했습니다.`n" -ForegroundColor:DarkRed
+    Write-Host " * 스크립트를 종료합니다. 아무 키나 누르십시오.`n" -NoNewline -ForegroundColor:Gray; [void][Console]::ReadKey($true)
+    exit 1
+}
+
+# window position and size
+wps -n:$t
+
+# title
+wt "글꼴"
+
+# font files
+$tff = "$temp\$file"
+$sff = "${env:SystemRoot}\Fonts\$file"
+$uff = "${env:LOCALAPPDATA}\Microsoft\Windows\Fonts\$file"
+
+# font registry keys
+$frk = "Microsoft\Windows NT\CurrentVersion\Fonts"
+$srk = "HKLM:\SOFTWARE\$frk"
+$urk = "HKCU:\Software\$frk"
+
+$f = "Yellow"
+
+# check system font file
+if (Test-Path $sff) {
+    wh " $file 설치" $f -n
+    e 0
+}
+
+# check user font file
+if (!(Test-Path $uff)) {
+    df $url $tff -e
+    $i = wh " $file 설치" $f -r
+    (New-Object -ComObject Shell.Application).Namespace(0x14).CopyHere($tff)
+    if (!(Test-Path $uff)) {
+        wh " 실패" DarkRed -n
+        wh "! 파일이 존재하지 않습니다." -n
+        wh "! $uff"
+        e 1
+    }
+}
+
+if (!$i) {
+    wh " $file 설치" $f
+}
+
+# check user registry font name
+$urn = (Get-ItemProperty $urk).PSObject.Properties | Where-Object Value -like "*$file" | Select-Object -ExpandProperty:Name
+if ($urn) {
+    # check system registry font name and file
+    if ((Get-ItemProperty $srk $urn -ErrorAction:SilentlyContinue) -and (Test-Path $sff)) {
+        e 0
+    }
+} else {
+    wh " 실패" DarkRed -n
+    wh "! 글꼴 이름이 존재하지 않습니다." -n
+    wh "! $tff"
+    e 1
+}
+
+# copy user font file to system font file
+se powershell.exe "Copy-Item '$uff' '$sff' -Force"
+if (!(Test-Path $sff)) {
+    wh " 실패" DarkRed -n
+    wh "! 파일이 존재하지 않습니다." -n
+    wh "! $sff"
+    e 1
+}
+
+# remove user font regisrty and file
+se powershell.exe "Stop-Service FontCache"
+Remove-ItemProperty $urk $urn
+se powershell.exe "Remove-Item $uff -Force"
+se powershell.exe "Start-Service FontCache"
+
+# add system registry font name
+se powershell.exe "New-ItemProperty '$srk' '$urn' -PropertyType:String -Value '$file'"
+$srn = (Get-ItemProperty $srk).PSObject.Properties | Where-Object Value -like "*$file" | Select-Object -ExpandProperty:Name
+if (!$srn) {
+    wh " 실패" DarkRed -n
+    wh "! 글꼴 이름이 존재하지 않습니다." -n
+    wh "! $srk"
+    e 1
+}
+if (!$e -and $srn -ne $urn) {
+    se powershell.exe "Remove-ItemProperty '$srk' '$srn'"
+    Remove-Item $sff -Force
+    wh " 실패" DarkRed -n
+    wh "! 글꼴 이름이 다릅니다." -n
+    wh "! $urn ≠ $srk"
+    e 1
+}
+
+wh -n
+if ($p) {
+    e 0
+}
---
+++ Font/readme.md
@@ -0,0 +1,30 @@
+# <img src="https://github.com/ssokka/Icons/blob/master/fonts.ico" width=30> 윈도우 글꼴 설치
+2020.05.16
+<br><br>
+
+## <img src="https://github.com/ssokka/Icons/blob/master/zip.ico" width=25> 자동 스크립트 다운로드
+
+### [D2Coding.zip](https://github.com/ssokka/Windows/blob/master/Font/D2Coding.zip?raw=true)
+<br>
+
+## <img src="https://github.com/ssokka/Icons/blob/master/powershell.ico" width=25> 파워셸 스크립트
+
+### font.ps1
+
+#### 명령 프롬프트 실행
+```
+powershell.exe -nop -ep bypass -f font.ps1 D2Coding.ttc https://raw.githubusercontent.com/ssokka/Fonts/master/D2Coding.ttc -m -r -p
+```
+
+#### 실행 옵션
+```
+-file : 글꼴 파일 .ttc .ttf 지원
+        기본 D2Coding.ttc
+-url  : 기본 https://raw.githubusercontent.com/ssokka/Fonts/master
+-m    : 필수 모듈 module.psm1 강제 다운로드
+-p    : 일시 정지 후 스크립트 종료
+-r    : 스크립트 작동 후 임시 작업 폴더 삭제
+-d    : 디버그 모드
+-t    : 테스트 모드
+        개발자용
+```
