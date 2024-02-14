48a296911abe55fde22084314e62e82c1dc9bdd2
ssokka <ssokka.root@gmail.com>
2024-02-14 17:27:51 +0900

    Update Event-Security-4625.ps1

--- Script/Event-Security-4625.ps1
+++ Script/Event-Security-4625.ps1
@@ -1,7 +1,10 @@
 echo ""
 echo "### Event - Security - ID 4625"
 $Events = Get-WinEvent -FilterHashtable @{LogName='Security';ID=4625} -ErrorAction Ignore
-if(! $?){ echo "Not Exist" }
+if(! $?){
+	echo ""
+	echo "Not Exist"
+}
 foreach($Event in $Events){
 	# Convert the event to XML
 	$XMLs = [xml]$Event.ToXml()
