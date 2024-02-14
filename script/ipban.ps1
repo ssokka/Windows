c68be3cbbf1df89ed2f4fea3ff1768fa759bb963
ssokka <ssokka.root@gmail.com>
2024-02-14 12:13:18 +0900

    Update ipban.ps1

--- script/ipban.ps1
+++ script/ipban.ps1
@@ -14,14 +14,23 @@ Stop-Service "IPBAN" -Force
 $file = "$dir\ipban.config"
 if (Test-Path "${file}") {
 	$xml = [xml](Get-Content "${file}")
+	echo "FailedLoginAttemptsBeforeBan = 4"
 	$node = $xml.configuration.appSettings.add | where {$_.key -eq 'FailedLoginAttemptsBeforeBan'}
 	$node.value = '4'
+	
+	echo "BanTime = 00:00:00:00"
 	$node = $xml.configuration.appSettings.add | where {$_.key -eq 'BanTime'}
 	$node.value = '00:00:00:00'
+	
+	echo "ExpireTime = 00:00:00:00"
 	$node = $xml.configuration.appSettings.add | where {$_.key -eq 'ExpireTime'}
 	$node.value = '00:00:00:00'
+	
+	echo "Whitelist = 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
 	$node = $xml.configuration.appSettings.add | where {$_.key -eq 'Whitelist'}
 	$node.value = '10.0.0.0/8,172.16.0.0/12,192.168.0.0/16'
+	
+	echo "UseDefaultBannedIPAddressHandler = false"
 	$node = $xml.configuration.appSettings.add | where {$_.key -eq 'UseDefaultBannedIPAddressHandler'}
 	$node.value = 'false'
 	$xml.Save("${file}")
