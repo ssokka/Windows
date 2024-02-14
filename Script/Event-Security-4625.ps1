2c911700a4623b0b87cc005a6aeb940ace0a790c
ssokka <ssokka.root@gmail.com>
2024-02-14 14:45:37 +0900

    Create Event-Security-4625.ps1

---
+++ Script/Event-Security-4625.ps1
@@ -0,0 +1,13 @@
+$Events = Get-WinEvent -FilterHashtable @{LogName='Security';ID=4625} -ErrorAction Ignore
+foreach($Event in $Events){
+	# Convert the event to XML
+	$XMLs = [xml]$Event.ToXml()
+	foreach($XML in $XMLs){
+		$Log = New-Object psobject -Property @{
+			Date = $Event.TimeCreated
+			UserName = $XML.Event.EventData.Data[5].'#text'
+			IP = $XML.Event.EventData.Data[19].'#text'
+		}
+		$Log
+	}
+}
