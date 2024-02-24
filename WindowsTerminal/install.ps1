7b1e58666388ebb7324ac7f0395aab445016e3d4
ssokka <ssokka.root@gmail.com>
2024-02-24 23:51:35 +0900

    update

--- WindowsTerminal/install.ps1
+++ WindowsTerminal/install.ps1
@@ -4,20 +4,19 @@ try {
 	$name = 'Windows 터미널'
 	$path = "$Env:ProgramFiles\WindowsApps\Microsoft.WindowsTerminal_1.18.10301.0_x64__8wekyb3d8bbwe"
 	$exec = "$path\wt.exe"
-	
+
 	Write-Host -f Green "`n### $name 설정"
 	$path = "$Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 	$file = 'settings.json'
 	$url = 'https://raw.githubusercontent.com/ssokka/Windows/master/WindowsTerminal'
     ni $path -it d -f -ea ig | Out-Null
 	Start-BitsTransfer "$url/$file" "$path\$file"
-    
-    
+
     Write-Host -f Green "`n### $name 설정 내용"
-	$str = [Text.Encoding]::UTF8.GetString(([Net.WebClient]::new()).DownloadData("$url\readme.md"))
+    $str = [Text.Encoding]::UTF8.GetString(([Net.WebClient]::new()).DownloadData("$url\readme.md"))
     $str -replace '(?is).*### 설정 내용.*?```(.*?)```.*','$1'
 
-	Write-Host -f Green "`n### 완료"
+    Write-Host -f Green "`n### 완료"
 }
 catch {
 	Write-Error ($_.Exception | fl -Force | Out-String)
