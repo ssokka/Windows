1abaa2964b05bde2872597f2adf1fcb6833ad1dc
ssokka <ssokka.root@gmail.com>
2024-02-26 02:34:51 +0900

    update

--- Activation/install.ps1
+++ Activation/install.ps1
@@ -6,31 +6,33 @@ try {
 	$name = 'Windows 정품 인증'
 	
 	$host.ui.RawUI.WindowTitle = $name
-    
-	if (!(cscript /Nologo "$Env:WinDir\System32\slmgr.vbs" /xpr) -match '정품 인증되었습니다') {
-        Write-Host -f Green "`n### $name"
-        if (!(gmo 7Zip4Powershell -l)) {
-            Set-ExecutionPolicy Bypass -f
-            $null = Install-PackageProvider NuGet -min 2.8.5.201 -Force
-            Register-PSRepository -d -ea ig
-            Set-PSRepository PSGallery -i Trusted
-            $null = inmo 7Zip4PowerShell -f
-        }
-        $file = 'restore.7z'
-        $url = "https://github.com/ssokka/Windows/raw/master/Activation/$file"
-        $zip = "$Env:TEMP\$file"
-        Start-BitsTransfer $url $zip
-        Write-Host -n "암호: "
-        $lline = 0
-        while ($true) {
-            $x, $y = [Console]::CursorLeft, [Console]::CursorTop
-            try { $test = $(Get-7Zip $zip -s ($pass = read-host -a)) } catch {}
-            if ($test) { break } else { ccp $x $y }
-        }
-        Expand-7Zip $zip $Env:TEMP -s $pass
-        ri $zip -Force -ea ig
-        start -n -wait restore.exe '/activate'
+	
+	if (!(( cscript /Nologo "$Env:WinDir\System32\slmgr.vbs" /xpr) -match '정품 인증되었 습다')) {
+		Write-Host -f Green "`n### $name"
+		if (!(gmo 7Zip4Powershell -l)) {
+			Set-ExecutionPolicy Bypass -f
+			$null = Install-PackageProvider NuGet -min 2.8.5.201 -Force
+			Register-PSRepository -d -ea ig
+			Set-PSRepository PSGallery -i Trusted
+			$null = inmo 7Zip4PowerShell -f
+		}
+		$file = 'restore.7z'
+		$url = "https://github.com/ssokka/Windows/raw/master/Activation/$file"
+		$zip = "$Env:TEMP\$file"
+		Start-BitsTransfer $url $zip
+		Write-Host -n "암호: "
+		$lline = 0
+		while ($true) {
+			$x, $y = [Console]::CursorLeft, [Console]::CursorTop
+			try { $test = $(Get-7Zip $zip -s ($pass = read-host -a)) } catch {}
+			if ($test) { break } else { ccp $x $y }
+		}
+		Expand-7Zip $zip $Env:TEMP -s $pass
+		ri $zip -Force -ea ig
+		start -n -wait restore.exe '/activate'
 	}
+	
+	Write-Host -f Green "`n### $name 확인"
 	cscript /Nologo "$Env:WinDir\System32\slmgr.vbs" /xpr
 
 	Write-Host -f Green "`n### 완료"
