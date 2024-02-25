1e145c9f80d5ce3be951e00b949916f7654a4488
ssokka <ssokka.root@gmail.com>
2024-02-25 16:55:49 +0900

    update

--- WindowsTerminal/install.ps1
+++ WindowsTerminal/install.ps1
@@ -1,34 +1,24 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿iex ([Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1')
 
-function Show-Process ($Process, [Switch]$Maximize) {
-	$sig = '
-	[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-	[DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);
-	'
-	if ($Maximize) { $Mode = 3 } else { $Mode = 4 }
-	$type = Add-Type -MemberDefinition $sig -Name WindowAPI -PassThru
-	$hwnd = $process.MainWindowHandle
-	$null = $type::ShowWindowAsync($hwnd, $Mode)
-	$null = $type::SetForegroundWindow($hwnd) 
-}
-Show-Process -Process ([System.Diagnostics.Process]::GetCurrentProcess()).ID
+$ErrorActionPreference = 'Stop'
 
 try {
 	$name = 'Windows 터미널'
 	$path = "$Env:ProgramFiles\WindowsApps\Microsoft.WindowsTerminal_1.18.10301.0_x64__8wekyb3d8bbwe"
 	$exec = "$path\wt.exe"
 	
-	$host.ui.RawUI.WindowTitle = $name
-	
     Write-Host -f Green "`n### $name 설정"
 	$path = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 	$file = 'settings.json'
 	$url = 'https://raw.githubusercontent.com/ssokka/Windows/master/WindowsTerminal'
+	
+	reg.exe add 'HKCU\Console\%%Startup' /v 'DelegationConsole' /t REG_SZ /d '{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}' /f | Out-Null
+	reg.exe add 'HKCU\Console\%%Startup' /v 'DelegationTerminal' /t REG_SZ /d '{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}' /f | Out-Null
+	
 	ni $path -it d -f -ea ig | Out-Null
 	Start-BitsTransfer "$url/$file" "$path\$file"
-	$str = ([Net.WebClient]::new()).DownloadString("$url\readme.md")
-	$str -replace '(?is).*?설정 내용.*```(?:\r\n|\n)(.*?)(?:\r\n|\n)```.*','$1'
-
+	([Net.WebClient]::new()).DownloadString("$url\readme.md") -replace '(?is).*?설정 내용.*```(?:\r\n|\n)(.*?)(?:\r\n|\n)```.*','$1'
+	
 	Write-Host -f Green "`n### 완료"
 }
 catch {
