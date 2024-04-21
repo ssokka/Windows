df251f5993324e3dc697446aec8106ed9afb9055
ssokka <ssokka.root@gmail.com>
2024-04-21 13:46:27 +0900

    Update install.ps1

--- Terminal/install.ps1
+++ Terminal/install.ps1
@@ -1,4 +1,4 @@
-﻿iex ([Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1')
+﻿Invoke-Expression ([Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1')
 
 $ErrorActionPreference = 'Stop'
 
@@ -7,7 +7,7 @@ try {
 	$path = "$Env:ProgramFiles\WindowsApps\Microsoft.WindowsTerminal_1.18.10301.0_x64__8wekyb3d8bbwe"
 	$exec = "$path\wt.exe"
 	
-    Write-Host -f Green "`n### $name 설정"
+    Write-Host -ForegroundColor Green "`n### $name 설정"
 	$path = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 	$file = 'settings.json'
 	$site = 'https://raw.githubusercontent.com/ssokka/Windows/master/Terminal'
@@ -15,15 +15,15 @@ try {
 	$null = reg.exe add 'HKCU\Console\%%Startup' /v 'DelegationConsole' /t REG_SZ /d '{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}' /f
 	$null = reg.exe add 'HKCU\Console\%%Startup' /v 'DelegationTerminal' /t REG_SZ /d '{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}' /f
 	
-	$null = ni $path -it d -f -ea ig
+	$null = New-Item $path -ItemType Directory -ErrorAction Ignore
 	Start-BitsTransfer "$site/$file" "$path\$file"
 	([Net.WebClient]::new()).DownloadString("$site\readme.md") -replace '(?is).*?설정.*?```(?:\r\n|\n)(.*?)(?:\r\n|\n)```.*','$1'
 	
-	Write-Host -f Green "`n### 완료"
+	Write-Host -ForegroundColor Green "`n### 완료"
 }
 catch {
-	Write-Error ($_.Exception | fl -Force | Out-String)
-	Write-Error ($_.InvocationInfo | fl -Force | Out-String)
+	Write-Error ($_.Exception | Format-List -Force | Out-String)
+	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String)
 }
 
 Write-Host -n "`n아무 키나 누르십시오..."
