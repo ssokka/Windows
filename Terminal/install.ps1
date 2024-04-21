9fb1bc95e2ad60655acd9c0260a455056782993e
ssokka <ssokka.root@gmail.com>
2024-04-21 22:11:04 +0900

    Update install.ps1

--- Terminal/install.ps1
+++ Terminal/install.ps1
@@ -1,5 +1,4 @@
 ﻿Invoke-Expression ([Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1')
-
 $ErrorActionPreference = 'Stop'
 
 try {
@@ -8,15 +7,16 @@ try {
 	$exec = "$path\wt.exe"
 	
     Write-Host -ForegroundColor Green "`n### $name 설정"
+	$site = 'https://raw.githubusercontent.com/ssokka/Windows/master/Terminal'
 	$path = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 	$file = 'settings.json'
-	$site = 'https://raw.githubusercontent.com/ssokka/Windows/master/Terminal'
 	
+	$null = New-Item "$path" -ItemType Directory -ErrorAction Ignore
+	Start-BitsTransfer "$site/$file" "$path\$file"
+
 	$null = reg.exe add 'HKCU\Console\%%Startup' /v 'DelegationConsole' /t REG_SZ /d '{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}' /f
 	$null = reg.exe add 'HKCU\Console\%%Startup' /v 'DelegationTerminal' /t REG_SZ /d '{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}' /f
 	
-	$null = New-Item $path -ItemType Directory -ErrorAction Ignore
-	Start-BitsTransfer "$site/$file" "$path\$file"
 	([Net.WebClient]::new()).DownloadString("$site\readme.md") -replace '(?is).*?설정.*?```(?:\r\n|\n)(.*?)(?:\r\n|\n)```.*','$1'
 	
 	Write-Host -ForegroundColor Green "`n### 완료"
