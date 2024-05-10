fc1cb0f0d2deaedd257bb7398aaf432fbde34f3c
ssokka <ssokka.root@gmail.com>
2024-05-10 12:10:52 +0900

    Add files via upload

--- WireGuard/wg.ps1
+++ WireGuard/wg.ps1
@@ -3,7 +3,7 @@ $path = "$Env:ProgramFiles\$name"
 $exec = "$path\$name.exe"
 $data = Get-ChildItem "$path\Data\Configurations\*.dpapi"
 
-$ErrorActionPreference = 'SilentlyContinue'
+$ErrorActionPreference = "SilentlyContinue"
 
 Add-Type @'
 using System;
@@ -31,7 +31,7 @@ function set-window {
 	param(
 		[int[]]$show = (1, 9)
 	)
-	$ErrorActionPreference = 'SilentlyContinue'
+	$ErrorActionPreference = "SilentlyContinue"
 	$ppid = (Get-WmiObject Win32_Process -Filter "processid='$PID'").ParentProcessId
 	$hwnd = (Get-Process -Id $ppid).MainWindowHandle
 	if (!$hwnd) { $hwnd = (Get-Process -Id $pid).MainWindowHandle }
@@ -46,111 +46,110 @@ function set-window {
 	$null = [Window]::SetForegroundWindow($hwnd)
 	$show | % { $null = [Window]::ShowWindow($hwnd, $_) }
 }
-
-function wg-service {
-	if (!(Test-Path $exec)) { exit }
-	set-window
-	#$host.ui.RawUI.WindowTitle = $(Get-PSCallStack)[0].FunctionName.ToUpper()
-	$host.ui.RawUI.WindowTitle = "$name"
-	Write-Host -ForegroundColor Green "`n### $name"
-	Write-Host -ForegroundColor Blue "`n# 서비스 확인"
-	
-	$sname = 'WireGuardManager'
-	if (!(Get-Service $sname -ErrorAction Ignore)) {
-		Start-Process -Verb RunAs -Wait -WindowStyle Hidden $exec "/installmanagerservice"
-		do {
-			Start-Sleep -Milliseconds 250
-		} until (Get-Service $sname)
-		(Get-Process | Where-Object { $_.ProcessName -eq "wireguard" -and $_.MainWindowTitle -eq "WireGuard" }).Kill()
-	}
-	Start-Process -Verb RunAs -Wait -WindowStyle Hidden sc.exe "failure $sname reset= 0 actions= restart/0/restart/0/restart/0"
-	Start-Process -Verb RunAs -Wait -WindowStyle Hidden sc.exe "start $sname"
-	do {
-		Start-Sleep -Milliseconds 250
-	} until ((Get-Service $sname).Status -eq 'Running')
-	
-	$data | ForEach-Object {
-		if($_) {
-			$sname = "WireGuardTunnel`$$($_ -replace '.*\\(.*)\.conf.*', '$1')"
-			if (!(Get-Service $sname -ErrorAction Ignore)) {
-				Start-Process -Verb RunAs -Wait -WindowStyle Hidden $exec "/installtunnelservice `"$_`""
-				do {
-					Start-Sleep -Milliseconds 250
-				} until (Get-Service $sname)
-			}
-			Start-Process -Verb RunAs -Wait -WindowStyle Hidden sc.exe "failure $sname reset= 0 actions= restart/0/restart/0/restart/0"
-		}
-	}
-}
-
-function wg-smb {
-	$reg = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2'
+function smb {
+	$reg = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2"
 	$smb = foreach($row in Get-SmbMapping){
-		$key = Join-Path -Path $reg -ChildPath ($row.RemotePath -replace '\\', '#')
+		$key = Join-Path -Path $reg -ChildPath ($row.RemotePath -replace '\\','#')
 		[pscustomobject]@{
 			RemotePath = $row.RemotePath
 			LocalPath = $row.LocalPath
 			Status = $row.Status
-			Label = Get-ItemPropertyValue -Path $key -Name '_LabelFromReg' -ErrorAction SilentlyContinue
+			Label = Get-ItemPropertyValue -Path $key -Name "_LabelFromReg" -ErrorAction SilentlyContinue
 		}
 	}
 if ($smb) { ($smb | Sort-Object -Property LocalPath | Out-String).Trim("`r","`n") }
 }
 
+function service {
+	if (!(Test-Path -Path $exec)) { exit }
+	set-window
+	#$host.ui.RawUI.WindowTitle = $(Get-PSCallStack)[0].FunctionName.ToUpper()
+	$host.ui.RawUI.WindowTitle = "$name"
+	Write-Host "`n### $name" -ForegroundColor Green
+	Write-Host "`n# 서비스 확인" -ForegroundColor Blue
+	
+	$sname = "WireGuardManager"
+	if (!(Get-Service -Name $sname -ErrorAction Ignore)) {
+		Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath $exec -ArgumentList "/installmanagerservice"
+		do {
+			Start-Sleep -Milliseconds 250
+		} until (Get-Service -Name $sname)
+		(Get-Process | Where-Object { $_.ProcessName -eq "wireguard" -and $_.MainWindowTitle -eq "WireGuard" }).Kill()
+	}
+	Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath sc.exe -ArgumentList "failure $sname reset= 0 actions= restart/0/restart/0/restart/0"
+	Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath sc.exe -ArgumentList "start $sname"
+	do {
+		Start-Sleep -Milliseconds 250
+	} until ((Get-Service -Name $sname).Status -eq "Running")
+	
+	$data | ForEach-Object {
+		if($_) {
+			$sname = "WireGuardTunnel`$$($_ -replace '.*\\(.*)\.conf.*', '$1')"
+			if (!(Get-Service -Name $sname -ErrorAction Ignore)) {
+				Start-Process -FilePath $exec -ArgumentList "/installtunnelservice `"$_`"" -Verb RunAs -Wait -WindowStyle Hidden
+				do {
+					Start-Sleep -Milliseconds 250
+				} until (Get-Service -Name $sname)
+			}
+			Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath sc.exe -ArgumentList "failure $sname reset= 0 actions= restart/0/restart/0/restart/0"
+		}
+	}
+}
+
 function on {
-	if (!(Test-Path $exec)) { exit }
-	wg-service
+	if (!(Test-Path -Path $exec)) { exit }
+	service
 	#$host.ui.RawUI.WindowTitle = $(Get-PSCallStack)[0].FunctionName.ToUpper()
 	$host.ui.RawUI.WindowTitle = "$name On"
-	Write-Host -ForegroundColor Green "`n### $name On"
-	Write-Host -ForegroundColor Blue "`n# 서비스 시작"
+	Write-Host "`n### $name On" -ForegroundColor Green
+	Write-Host "`n# 서비스 시작" -ForegroundColor Blue
 	(Get-Service -Name "WireGuardTunnel`$*" -ErrorAction Ignore).Name | ForEach-Object {
 		if($_) {
 			$_
-			Start-Process -Verb RunAs -Wait -WindowStyle Hidden sc.exe "start `"$_`""
+			Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath sc.exe -ArgumentList "start `"$_`""
 			do {
 				Start-Sleep -Milliseconds 250
-			} until ((Get-Service "$_").Status -eq 'Running')
+			} until ((Get-Service -Name "$_").Status -eq "Running")
 		}
 	}
 	$file = "$PSScriptRoot\drive.cmd"
-	if (Test-Path $file) {
-		Write-Host -ForegroundColor Blue "`n# 네트워크 드라이브 연결"
-		Start-Process -Wait -NoNewWindow $file
+	if (Test-Path -Path $file) {
+		Write-Host "`n# 네트워크 드라이브 연결" -ForegroundColor Blue
+		Start-Process -NoNewWindow -Wait -FilePath $file
 		#(Get-SmbMapping | Sort-Object | Format-Table -Force | Out-String).Trim("`r","`n")
-		wg-smb
+		smb
 	}
 	Write-Host
-	foreach ($i in 5..1) { Write-Host -NoNewline "`r${i}초 후 자동 닫힘"; Start-Sleep 1 }
+	foreach ($i in 5..1) { Write-Host "`r${i}초 후 자동 닫힘" -NoNewline; Start-Sleep 1 }
 }
 
 function off {
-	if (!(Test-Path $exec)) { exit }
+	if (!(Test-Path -Path $exec)) { exit }
 	set-window
 	#$host.ui.RawUI.WindowTitle = $(Get-PSCallStack)[0].FunctionName.ToUpper()
 	$host.ui.RawUI.WindowTitle = "$name Off"
-	Write-Host -ForegroundColor Green "`n### $name Off"
-	Write-Host -ForegroundColor Blue "`n# 네트워크 드라이브 연결 끊기"
-	(Get-NetIPAddress -InterfaceAlias "wg*").IPAddress -replace '(.*\.).*', '$1' | ForEach-Object { (Get-SmbMapping -RemotePath "*$_*").LocalPath } | Sort-Object | ForEach-Object { if ($_) { $null = net use $_ /delete /y 2>$null } }
-	wg-smb
-	Write-Host -ForegroundColor Blue "`n# 서비스 중지"
+	Write-Host "`n### $name Off" -ForegroundColor Green
+	Write-Host "`n# 네트워크 드라이브 연결 끊기" -ForegroundColor Blue
+	(Get-NetIPAddress -InterfaceAlias "wg*").IPAddress -replace '(.*\.).*','$1' | ForEach-Object { (Get-SmbMapping -RemotePath "*$_*").LocalPath } | Sort-Object | ForEach-Object { if ($_) { $null = net use $_ /delete /y 2>$null } }
+	smb
+	Write-Host "`n# 서비스 중지" -ForegroundColor Blue
 	(Get-Service -Name "WireGuardTunnel`$*" -ErrorAction Ignore).Name | ForEach-Object {
 		if($_) {
 			$_
-			Start-Process -Verb RunAs -Wait -WindowStyle Hidden sc.exe "stop `"$_`""
+			Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath sc.exe -ArgumentList "stop `"$_`""
 			do {
 				Start-Sleep -Milliseconds 250
-			} until ((Get-Service "$_").Status -eq 'Stopped')
+			} until ((Get-Service -Name "$_").Status -eq 'Stopped')
 		}
 	}
-	$sname = 'WireGuardManager'
-	Start-Process -Verb RunAs -Wait -WindowStyle Hidden sc.exe "stop `"$sname`""
+	$sname = "WireGuardManager"
+	Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath sc.exe -ArgumentList "stop `"$sname`""
 	do {
 		Start-Sleep -Milliseconds 250
-	} until ((Get-Service "$sname").Status -eq 'Stopped')
+	} until ((Get-Service -Name "$sname").Status -eq "Stopped")
 	
 	Write-Host
-	foreach ($i in 5..1) { Write-Host -NoNewline "`r${i}초 후 자동 닫힘"; Start-Sleep 1 }
+	foreach ($i in 5..1) { Write-Host "`r${i}초 후 자동 닫힘" -NoNewline; Start-Sleep 1 }
 }
 
 function drive {
@@ -162,17 +161,17 @@ function drive {
 		[String]$pass,
 		[String]$name
 	)
-	if (!(Test-Path $exec)) { exit }
+	if (!(Test-Path -Path $exec)) { exit }
 	set-window
-	Write-Host -ForegroundColor Green "`n### $name 네트워크 드라이브 연결"
+	Write-Host "`n### $name 네트워크 드라이브 연결" -ForegroundColor Green
 	do {
 		Start-Sleep -Milliseconds 250
 		$temp = [Net.Dns]::GetHostAddresses($ipad)
 	} until ($temp)
-	$ipad = ($temp | ? { $_.AddressFamily -eq 'InterNetwork' }).IPAddressToString
+	$ipad = ($temp | ? { $_.AddressFamily -eq "InterNetwork" }).IPAddressToString
 	do {
 		Start-Sleep -Milliseconds 250
-	} until (Test-Connection $ipad -Count 1)
+	} until (Test-Connection -TargetName $ipad -Count 1)
 	$null = net use $ndrv /delete /y 2>$null
 	$null = net use $ndrv "\\$ipad\$path" /persistent:yes /user:$user $pass 2>$null
 	if (!$?) { net use $ndrv "\\$ipad\$path" /persistent:yes }
@@ -181,6 +180,7 @@ function drive {
 	} else {
 		powershell -WindowStyle Normal -Command { exit }
 		set-window
-		Write-Host -NoNewline "`n아무 키나 누르십시오..."; Read-Host
+		Write-Host "`n아무 키나 누르십시오..." -NoNewline
+		Read-Host
 	}
 }
