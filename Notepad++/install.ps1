014cad65ae5d4774b083f0a206d28737711d938d
ssokka <ssokka.root@gmail.com>
2024-05-12 18:51:30 +0900

    Add files via upload

--- Notepad++/install.ps1
+++ Notepad++/install.ps1
@@ -1,86 +1,89 @@
 ﻿iex ([Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1')
 
-$ErrorActionPreference = 'Stop'
-
 try {
-	$name = 'Notepad++'
+	$name = "Notepad++"
 	$path = "$Env:ProgramFiles\$name"
-	$exec = "$path\notepad++.exe"
+	$exec = "$path\$name.exe"
+	
+	$site = "https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest"
+	$surl = (Invoke-RestMethod -Uri $site | ForEach-Object assets | Where-Object name -like '*.x64.exe').browser_download_url
+	
+	$gurl = "https://raw.githubusercontent.com/ssokka/Windows/master/$name"
 	
 	$host.ui.RawUI.WindowTitle = $name
-
-	Write-Host -f Green "`n### $name 버전"
-	$cver = "$((gi $exec -ea ig).VersionInfo.FileVersion)".Trim()
-	$site = "https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest"
-	$rver = "$(((irm $site).tag_name) -replace '(?i)v','')".Trim()
-	$rurl = (irm $site | % assets | ? name -like '*.x64.exe').browser_download_url
-	Write-Host "현재: $cver"
-	Write-Host "최신: $rver"
+	Write-Host "`n### $name" -ForegroundColor Green
 	
-	if ($cver -ne $rver) {
-		Write-Host -f Green "`n### $name 설치"
-		$file = "$Env:TEMP\$($rurl -replace '.*/(.*)','$1')"
-		Start-BitsTransfer $rurl $file
-		spps -n $name -f -ea ig
-		start -n -wait $file '/S'
-		ri $file -Force -ea ig
+	Write-Host "`n# 버전" -ForegroundColor Blue
+	$cver = "$((Get-Item -Path $exec -ErrorAction Ignore).VersionInfo.FileVersion)".Trim()
+	$sver = "$(((Invoke-RestMethod -Uri $site).tag_name) -replace '(?i)v','')".Trim()
+	Write-Host "현재: $cver"
+	Write-Host "최신: $sver"
+	
+	if ($cver -ne $sver) {
+		Write-Host "`n# 다운로드" -ForegroundColor Blue
+		$file = "$Env:Temp\$($surl -replace '.*/(.*)', '$1')"
+		Start-BitsTransfer -Source $surl -Destination $file
+		Write-Host "`n# 설치" -ForegroundColor Blue
+		Stop-Process -Name $name -Force -ErrorAction Ignore
+		Start-Process -NoNewWindow -Wait -FilePath $file -ArgumentList "/S"
+		Remove-Item -Path $file -Force -ErrorAction Ignore
 	}
-
-	spps -n $name -f -ea ig
-
-	Write-Host -f Green "`n### $name 플러그인 설치"
+	
 	# https://github.com/notepad-plus-plus/nppPluginList/blob/master/doc/plugin_list_x64.md
+	Write-Host "`n# 플러그인 설치" -ForegroundColor Blue
+	Stop-Process -Name $name -Force -ErrorAction Ignore
 	function InstallPlugin {
 		[Alias("ip")]
 		param(
 			[Parameter(Mandatory=$true)]
-			[string]$n,	# folder
+			[string]$p,	# path
 			[string]$r,	# repos
 			[string]$t	# title
 		)
-		$ErrorActionPreference = 'Ignore'
-		if(!(Test-Path "$path\plugins\$n")){
+		$ErrorActionPreference = "Ignore"
+		if(!(Test-Path -Path "$path\plugins\$p\*.dll")){
 			Write-Host "$t"
-			$null = ni "$path\plugins\$n" -it d -ea ig
+			$null = New-Item -Path "$path\plugins\$p" -ItemType Directory
 			if ($r -match '^http') {
 				$req = [Net.WebRequest]::Create($r)
 				$req.AllowAutoRedirect = $true
-				$res = $req.GetResponse()
-				$gfn = [IO.Path]::GetFileName($res.ResponseUri.AbsoluteUri)
+				$gfn = [IO.Path]::GetFileName($req.GetResponse().ResponseUri.AbsolutePath)
 				$rurl = $r
-				$file = "$path\plugins\$n\$gfn"
+				$file = "$path\plugins\$p\$gfn"
 			} else {
-				$rurl = (irm https://api.github.com/repos/$r/releases/latest | % assets | ? name -like '*x64.zip').browser_download_url
-				$file = "$path\plugins\$n\$($rurl -replace '.*/(.*)','$1')"
+				$rurl = (Invoke-RestMethod -Uri https://api.github.com/repos/$r/releases/latest | ForEach-Object assets | Where-Object name -like '*x64.zip').browser_download_url
+				$file = "$path\plugins\$p\$($rurl -replace '.*/(.*)', '$1')"
 			}
-			Start-BitsTransfer $rurl $file
-			Expand-Archive $file -d "$path\plugins\$n" -f
-			ri $file -Force
+			Start-BitsTransfer -Source $rurl -Destination $file
+			Expand-Archive -Path $file -DestinationPath "$path\plugins\$p" -Force
+			Remove-Item -Path $file -Force
 		}
 	}
-	ip 'ComparePlus' 'pnedev/ComparePlus' 'ComparePlus'
-	ip '_CustomizeToolbar' 'https://sourceforge.net/projects/npp-customize/files/latest/download' 'Customize Toolbar'
-	ip 'Explorer' 'oviradoi/npp-explorer-plugin' 'Explorer'
-	ip 'HexEditor' 'chcg/NPP_HexEdit' 'HexEditor'
-	ip 'NppExec' 'd0vgan/nppexec' 'NppExec'
-	ip 'NPPJSONViewer' 'kapilratnani/JSON-Viewer' 'JSON Viewer'
-	ip 'XMLTools' 'morbac/xmltools' 'XML Tools'
+	ip "ComparePlus" "pnedev/ComparePlus" "ComparePlus"
+	ip "_CustomizeToolbar" "https://sourceforge.net/projects/npp-customize/files/latest/download" "Customize Toolbar"
+	ip "Explorer" "oviradoi/npp-explorer-plugin" "Explorer"
+	ip "HexEditor" "chcg/NPP_HexEdit" "HexEditor"
+	ip "NppExec" "d0vgan/nppexec" "NppExec"
+	ip "NPPJSONViewer" "kapilratnani/JSON-Viewer" "JSON Viewer"
+	ip "XMLTools" "morbac/xmltools" "XML Tools"
 	
-	Write-Host -f Green "`n### $name 설정"
-	$site = 'https://raw.githubusercontent.com/ssokka/Windows/master/Notepad%2B%2B'
-	
+	Write-Host "`n# 설정" -ForegroundColor Blue
+	$file = "config.xml"
 	$path = "$Env:AppData\$name"
-	$file = 'config.xml'
-	$null = ni $path -it d -f -ea ig
-	Start-BitsTransfer "$site/$file" "$path\$file"
-
+	$null = New-Item -Path $path -ItemType Directory -ErrorAction Ignore
+	Start-BitsTransfer -Source "$gurl/$file" -Destination "$path\$file"
+	$file = "Dracula.xml"
 	$path = "$Env:AppData\$name\themes"
-	$file = 'Dracula.xml'
-	$null = ni $path -it d -f -ea ig
-	Start-BitsTransfer "https://raw.githubusercontent.com/dracula/notepad-plus-plus/master/$file" "$path\$file"
-
-	([Net.WebClient]::new()).DownloadString("$site\readme.md") -replace '(?is).*?설정.*?```(?:\r\n|\n)(.*?)(?:\r\n|\n)```.*','$1'
-
+	$null = New-Item -Path $path -ItemType Directory -ErrorAction Ignore
+	Start-BitsTransfer -Source "https://raw.githubusercontent.com/dracula/notepad-plus-plus/master/$file" -Destination "$path\$file"
+	$file = "plugins-config.zip"
+	$path = "$Env:AppData\$name\plugins\config"
+	$null = New-Item -Path $path -ItemType Directory -ErrorAction Ignore
+	Start-BitsTransfer -Source "$gurl/$file" -Destination "$path\$file"
+	Expand-Archive -Path "$path\$file" -DestinationPath "$path" -Force
+	Remove-Item -Path "$path\$file" -Force
+	([Net.WebClient]::new()).DownloadString("$gurl/readme.md") -replace '(?is).*?설정.*?```(?:\r\n|\n)(.*?)(?:\r\n|\n)```.*', '$1'
+	
 	#$xml = [xml](Get-Content '$path\themes\$file')
 	#$node = $xml.NotepadPlus.GlobalStyles.WidgetStyle | where {$_.name -eq 'Global override'}
 	#$node.fontSize = '10'
@@ -95,12 +98,14 @@ try {
 	#reg add 'HKCU\Software\Classes\.txt' /ve /t REG_SZ /d 'Notepad++_file' /f
 	#reg add 'HKCU\Software\Classes\.txt' /v 'Notepad++_backup' /t REG_SZ /d 'txtfile' /f
 
-	Write-Host -f Green "`n### 완료"
+	set-window
+	Write-Host "`n### 완료" -ForegroundColor Green
 }
 catch {
-	Write-Error ($_.Exception | fl -Force | Out-String)
-	Write-Error ($_.InvocationInfo | fl -Force | Out-String)
+	Write-Error ($_.Exception | Format-List -Force | Out-String) -ErrorAction Continue
+	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String) -ErrorAction Continue
+	throw
 }
 
-Write-Host -n "`n아무 키나 누르십시오..."
+Write-Host "`n아무 키나 누르십시오..." -NoNewline
 Read-Host
