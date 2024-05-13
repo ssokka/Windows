8d7d7c8c3827d3afc2c588d1652b06433b13e8bb
ssokka <ssokka.root@gmail.com>
2024-05-13 20:46:09 +0900

    update

---
+++ Bandizip/install.ps1
@@ -0,0 +1,51 @@
+﻿Invoke-Expression -Command ([Net.WebClient]::new()).DownloadString("https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1")
+
+try {
+	$name = "Bandizip"
+	$path = "$Env:ProgramFiles\$name"
+	$exec = "$path\$name.exe"
+	$gurl = "https://raw.githubusercontent.com/ssokka/Windows/master/$name"
+	
+	$site = "https://kr.bandisoft.com/bandizip"
+	$surl = "$site/dl.php?web"
+    $sexe = "BANDIZIP-SETUP-STD-X64.EXE"
+    $spat = '(?is).*?<h2><.*?>v*(.*?)<.*'
+	
+	$host.ui.RawUI.WindowTitle = $name
+	Write-Host "`n### $name" -ForegroundColor Green
+	
+	Write-Host "`n# 버전" -ForegroundColor Blue
+	$cver = "$((Get-Item -Path $exec -ErrorAction Ignore).VersionInfo.FileVersion -replace '(.*)\.0.*', '$1')".Trim()
+	$wc = New-Object System.Net.WebClient
+	$wc.Headers["User-Agent"] = $UserAgent
+	$sver = "$($wc.DownloadString("$site/history") -replace $spat, '$1')".Trim()
+	Write-Host "현재: $cver"
+	Write-Host "최신: $sver"
+	
+	if ($cver -ne $sver) {
+		Write-Host "`n# 다운로드" -ForegroundColor Blue
+		$file = "$Env:Temp\$sexe"
+		Start-BitsTransfer -Source $surl -Destination $file
+		Write-Host "`n# 설치" -ForegroundColor Blue
+		Stop-Process -Name $name -Force -ErrorAction Ignore
+		Start-Process -NoNewWindow -Wait -FilePath $file -ArgumentList "/S"
+		Remove-Item -Path $file -Force -ErrorAction Ignore
+	}
+	
+	Write-Host "`n# 설정" -ForegroundColor Blue
+	Start-BitsTransfer -Source "$gurl/$name.reg" -Destination "$Env:Temp\$name.reg"
+    Start-Process -NoNewWindow -Wait -FilePath regedit.exe -ArgumentList "/s `"$Env:Temp\$name.reg`""
+    Remove-Item -Path "$Env:Temp\$name.reg" -Force -ErrorAction Ignore
+    ([Net.WebClient]::new()).DownloadString("$gurl/readme.md") -replace '(?is).*?### 설정.*?```(?:\r\n|\n)(.*?)(?:\r\n|\n)```.*', '$1'
+	
+    set-window
+	Write-Host "`n### 완료" -ForegroundColor Green
+}
+catch {
+	Write-Error ($_.Exception | Format-List -Force | Out-String) -ErrorAction Continue
+	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String) -ErrorAction Continue
+	throw
+}
+
+Write-Host "`n아무 키나 누르십시오..." -NoNewline
+Read-Host
---
+++ Bandizip/readme.md
@@ -0,0 +1,31 @@
+﻿### [CMD] 반디집 설치
+```
+set _url=https://raw.githubusercontent.com/ssokka/Windows/master/Bandizip/install.ps1
+powershell start -v runas wt 'powershell iex ([Net.WebClient]::new()).DownloadString(''%_url%'')'
+
+```
+
+### 설정
+```
+> 일반 설정
+  [v] 자석 기능 사용
+> 탐색기 메뉴
+  [v] 여기에 풀고 삭제하기
+  [v] 알아서 풀고 삭제하기
+  [v] 관리자 권한으로 압축하기
+  [v] 명령 창(cmd) 열기 (Shift 키를 누르면 관리자 권한으로 실행 가능)
+  [v] 탐색기 메뉴를 계단식으로 보여주기
+  [v] 파일 확장자에 상관없이 파일 내용으로 압축 파일 여부 판단
+> 압축 풀기
+  [ ] 압축 풀기 완료 후 창을 닫지 않기
+> 압축 하기
+  [ ] 압축 풀기 완료 후 창을 닫지 않기
+  압축 방법: 압축률 최대
+> 보기/편집
+  파일 목록의 글꼴 바꾸기: Consolas
+  트리 컨트롤의 글꼴 바꾸기: Consolas
+  압축 파일 설명 창의 글꼴 바꾸기: Consolas
+  편집기: C:\Program Files\Notepad++\notepad++.exe
+> 고급 설정
+  [v] 압축 파일을 열 때 탐색기에서 삭제 가능한 상태로 열기
+```
