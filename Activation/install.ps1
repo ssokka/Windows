66d706cd404b1120ae0d0f93d1ff2c147380f2d4
ssokka <ssokka.root@gmail.com>
2024-05-13 16:48:00 +0900

    Add files via upload

--- Activation/install.ps1
+++ Activation/install.ps1
@@ -1,41 +1,45 @@
-﻿Invoke-Expression ([Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1')
-$ErrorActionPreference = 'Stop'
+﻿Invoke-Expression -Command ([Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/ssokka/Windows/master/Script/ps/header.ps1')
 
 try {
 	$name = 'Windows 정품 인증'
+	
 	$host.ui.RawUI.WindowTitle = $name
-	Write-Host -ForegroundColor Green "`n### $name"
-
+	Write-Host "`n### $name" -ForegroundColor Green
+	
 	$slmgr = "$Env:WinDir\System32\slmgr.vbs"
-	$str = ("$(cscript /Nologo "$slmgr" /xpr)" -replace '.*?(컴퓨터.*)','$1').Trim()
-	if (!($str -match '인증되었습니다')) {
-		$site = 'https://github.com/ssokka/Windows/raw/master/Activation'
-		$file = 'restore.7z'
-		$path = "$Env:Temp\Download"
-		$exec = 'restore.exe'
-		$null = New-Item "$path" -ItemType Directory -ErrorAction Ignore
+	$str = ("$(cscript /Nologo "$slmgr" /xpr)" -replace '.*?(컴퓨터.*)', '$1').Trim()
+	if (!($str -match "인증되었습니다")) {
+		$site = "https://github.com/ssokka/Windows/raw/master/Activation"
+		$file = "restore.7z"
+		$exec = "restore.exe"
 		install-7zip
  		ddr $false
-		Start-BitsTransfer "$site/$file" "$path\$file"
-		Write-Host -NoNewline '암호: '
-		$lline = 0
+		Start-BitsTransfer -Source "$site/$file" -Destination "$Global:Temp\$file"
+		Write-Host "암호: " -NoNewline
+		$LastConsoleLine = 0
 		while ($true) {
 			$x, $y = [Console]::CursorLeft, [Console]::CursorTop
-			try { $test = $(Get-7Zip "$path\$file" -s ($pass = Read-Host -AsSecureString)) } catch {}
-			if ($test) { break } else { ccp $x $y }
+			Expand-7Zip -ArchiveFileName "$Global:Temp\$file" -TargetPath $Global:Temp -SecurePassword (Read-Host -AsSecureString) -ErrorAction Ignore
+			if ($?) {
+				break
+			} else {
+				ccp $x $y
+			}
 		}
-		Expand-7Zip "$path\$file" "$path" -SecurePassword $pass
-		Start-Process -NoNewWindow -Wait "$path\$exec" '/activate'
-		Remove-Item "$path" -Recurse -Force -ErrorAction Ignore
+		Start-Process -NoNewWindow -Wait "$Global:Temp\$exec" '/activate'
+		("$Global:Temp\$file", "$Global:Temp\$exec") | ForEach-Object { Remove-Item -Path $_ -Force -ErrorAction Ignore }
 		ddr $true
 	}
-	("$(cscript /Nologo "$slmgr" /xpr)" -replace '.*?(컴퓨터.*)','$1').Trim()
-	Write-Host -ForegroundColor Green "`n### 완료"
+	("$(cscript /Nologo "$slmgr" /xpr)" -replace '.*?(컴퓨터.*)', '$1').Trim()
+	
+	set-window
+	Write-Host "`n### 완료" -ForegroundColor Green
 }
 catch {
-	Write-Error ($_.Exception | Format-List -Force | Out-String)
-	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String)
+	Write-Error ($_.Exception | Format-List -Force | Out-String) -ErrorAction Continue
+	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String) -ErrorAction Continue
+	throw
 }
 
-Write-Host -NoNewline "`n아무 키나 누르십시오..."
+Write-Host "`n아무 키나 누르십시오..." -NoNewline
 Read-Host
