97d8a3f61f81e4fc82f230f0407bf8b045f5ad3f
ssokka <ssokka.root@gmail.com>
2024-05-14 14:44:25 +0900

    update

--- Script/ps/header.ps1
+++ Script/ps/header.ps1
@@ -1,6 +1,4 @@
-﻿param([bool]$wait = $true)
-
-Write-Host "`n### 준비중" -ForegroundColor Green -NoNewline
+﻿Write-Host "`n### 준비중" -ForegroundColor Green -NoNewline
 
 $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 
