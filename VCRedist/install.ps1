30c2f802b71f75a5a0564ffb594f715ea933d2ef
ssokka <ssokka.root@gmail.com>
2024-05-14 14:25:07 +0900

    update

--- VCRedist/install.ps1
+++ VCRedist/install.ps1
@@ -4,15 +4,14 @@ try {
 	$name = "Visual C++ 재배포 가능 패키지"
 	
 	$site = "https://api.github.com/repos/abbodi1406/vcredist/releases/latest"
-	$surl = (Invoke-RestMethod -Uri $site | ForEach-Object assets | Where-Object name -like '*x64.exe').browser_download_url
-	
-    $inst = "$Global:Temp\$($surl -replace '.*/(.*)', '$1')"
+	$down = (Invoke-RestMethod -Uri $site | ForEach-Object assets | Where-Object name -like '*x64.exe').browser_download_url
+    $inst = "$Global:Temp\$($down -replace '.*/(.*)', '$1')"
 	
 	$host.ui.RawUI.WindowTitle = $name
 	Write-Host "`n### $name" -ForegroundColor Green
 	
 	Write-Host "`n# 다운로드" -ForegroundColor Blue
-    Start-BitsTransfer -Source $surl -Destination $inst
+    Start-BitsTransfer -Source $down -Destination $inst
 	
     Write-Host "`n# 설치" -ForegroundColor Blue
     ("/aiR /y", "/y") | ForEach-Object { Start-Process -NoNewWindow -Wait -FilePath $inst -ArgumentList $_ }
@@ -21,12 +20,11 @@ try {
 	
 	set-window
 	Write-Host "`n### 완료" -ForegroundColor Green
+	
+	if ($wait) { Write-Host "`n아무 키나 누르십시오..." -NoNewline; Read-Host }
 }
 catch {
 	Write-Error ($_.Exception | Format-List -Force | Out-String) -ErrorAction Continue
 	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String) -ErrorAction Continue
 	throw
 }
-
-Write-Host "`n아무 키나 누르십시오..." -NoNewline
-Read-Host
