9480917845a2af6bb4fb575a2870244b57215f00
ssokka <ssokka.root@gmail.com>
2024-05-16 15:08:18 +0900

    update

--- header.ps1
+++ header.ps1
@@ -4,13 +4,16 @@ $x, $y = [Console]::CursorLeft, [Console]::CursorTop
 Write-Host "`n### 준비중" -ForegroundColor Green -NoNewline
 
 $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-$Git = "https://raw.githubusercontent.com/ssokka/Windows/master"
 
-$sb = { 'ConsentPromptBehaviorAdmin', 'PromptOnSecureDesktop' | ForEach-Object { reg.exe add 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' /v $_ /t REG_DWORD /d '0' /f } }
-Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath powershell.exe -ArgumentList "-command", "(Invoke-Command -ScriptBlock {$sb})"
+$Git = "https://raw.githubusercontent.com/ssokka/Windows/master"
 
 $Temp = "$Env:Temp\Download"
 New-Item -Path $Temp -ItemType Directory -Force | Out-Null
+
+$UserInput = Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern bool BlockInput(bool fBlockIt);' -Name UserInput -Namespace UserInput -PassThru
+
+$sb = { 'ConsentPromptBehaviorAdmin', 'PromptOnSecureDesktop' | ForEach-Object { reg.exe add 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' /v $_ /t REG_DWORD /d '0' /f } }
+Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath powershell.exe -ArgumentList "-command", "(Invoke-Command -ScriptBlock {$sb})"
 
 Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath powershell.exe -ArgumentList "-command", "
 Add-MpPreference -ExclusionPath `"$Temp`" -Force
@@ -18,7 +21,18 @@ Set-MpPreference -MAPSReporting 0 -Force
 Set-MpPreference -SubmitSamplesConsent 2 -Force
 "
 
-$UserInput = Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern bool BlockInput(bool fBlockIt);' -Name UserInput -Namespace UserInput -PassThru
+function install-7zip {
+	$mod = "7Zip4Powershell"
+	if (Get-Module -Name $mod -ListAvailable) { return }
+	Write-Host "`n# Powershell 7Zip 모듈 설치" -ForegroundColor Blue
+	Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath powershell.exe -ArgumentList "-command", "
+	Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+	Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force
+	Register-PSRepository -Default -ErrorAction Ignore
+	Set-PSRepository PSGallery -InstallationPolicy Trusted
+	Install-Module $mod -Force
+	"
+}
 
 function current-cursor-position {
 	[Alias("ccp")]
@@ -29,34 +43,6 @@ function current-cursor-position {
 	[Console]::SetCursorPosition($x, $y)
 	[Console]::Write("{0,-$([Console]::WindowWidth)}" -f " ")
 	[Console]::SetCursorPosition($x, $y)
-}
-
-function download {
-	[Alias("dw")]
-	param(
-		[string]$url,
-		[string]$dst = $Temp,
-		[string]$pat = '*x64.zip'
-	)
-	if ($url -match '^https://api.github.com/repos/.*/releases/latest$') {
-		$src = (Invoke-RestMethod -Uri $url | ForEach-Object assets | Where-Object name -like $pat).browser_download_url
-		$file = $src -replace '.*/(.*)', '$1'
-	} else {
-		$wr = [Net.WebRequest]::Create($url)
-		$wr.AllowAutoRedirect = $true
-		$wr.Method = "HEAD"
-		$re = $wr.GetResponse()
-		$src = $re.ResponseUri
-		$file = [IO.Path]::GetFileName($src.AbsolutePath)
-		$re.Close()
-	}
-	if ($dst.EndsWith('\')) {
-		$dst += $file
-	} else {
-		$dst += '\' + $file
-	}
-	Start-BitsTransfer -Source $src -Destination $dst
-	if (Test-Path -Path $dst) { return $dst }
 }
 
 function disable-defender-realtime {
@@ -81,6 +67,51 @@ function disable-defender-realtime {
 	}
 }
 
+function download {
+	[Alias("dw")]
+	param(
+		[string]$url,
+		[string]$dst = $Temp,
+		[string]$tmp = $Temp,
+		[string]$ren = '',
+		[string]$pat = '*x64.zip'
+	)
+	Write-Host "`n# 다운로드" -ForegroundColor Blue
+	if ($url -match '^https://api.github.com/repos/.*/releases/latest$') {
+		$src = (Invoke-RestMethod -Uri $url | ForEach-Object assets | Where-Object name -like $pat).browser_download_url
+		$file = $src -replace '.*/(.*)', '$1'
+	} else {
+		$wr = [Net.WebRequest]::Create($url)
+		$wr.AllowAutoRedirect = $true
+		$wr.Method = "HEAD"
+		$re = $wr.GetResponse()
+		$src = $re.ResponseUri
+		$file = [IO.Path]::GetFileName($src.AbsolutePath)
+		$re.Close()
+	}
+	if ($ren) { $file = $ren }
+	if ($dst.EndsWith('\')) {
+		$dst += $file
+	} else {
+		$dst += '\' + $file
+	}
+	Start-BitsTransfer -Source $src -Destination $dst
+	if (!(".zip", ".7z", ".rar" -contains (Get-ChildItem $dst).Extension )) { return $dst }
+	install-7zip
+	try { $test = Get-7Zip -ArchiveFileName $dst } catch {}
+	if ($test) {
+		Expand-7Zip -ArchiveFileName $dst -TargetPath $tmp -ErrorAction Ignore
+	} else {
+		Write-Host "암호: " -NoNewline
+		$LastConsoleLine = 0
+		while ($true) {
+			$x, $y = [Console]::CursorLeft, [Console]::CursorTop
+			Expand-7Zip -ArchiveFileName $dst -TargetPath $tmp -SecurePassword (Read-Host -AsSecureString) -ErrorAction Ignore
+			if ($?) { break } else { ccp $x $y }
+		}
+	}
+}
+
 function get-online-version {
 	[Alias("gov")]
 	param(
@@ -99,19 +130,6 @@ function get-online-version {
 	if ($ver) { return $ver.Trim() }
 }
 
-function install-7zip {
-	$mod = "7Zip4Powershell"
-	if (!(Get-Module -Name $mod -ListAvailable)) {
-		Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath powershell.exe -ArgumentList "-command", "
-		Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
-		Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force
-		Register-PSRepository -Default -ErrorAction Ignore
-		Set-PSRepository PSGallery -InstallationPolicy Trusted
-		Install-Module $mod -Force
-		"
-	}
-}
-
 function pin-to {
 	[Alias('pt')]
 	param(
@@ -128,16 +146,15 @@ function pin-to {
 		start { $type, $pdir = "51201", "$Env:AppData\Microsoft\Windows\Start Menu\Programs" }
 	}
 	if (!$icon) { $icon = $path }
-	$exec = "syspin.exe"
-	$down = "https://www.technosys.net/download.aspx?file=$exec"
+	$exec = "$Temp\syspin.exe"
 	$guid = ([System.Guid]::NewGuid()).ToString()
 	$gexe = "$Temp\$guid.exe"
 	$glnk = "$pdir\$guid.lnk"
 	$nlnk = "$pdir\$name.lnk"
 	Write-Host $name
-	if (!(Test-Path -Path "$Temp\$exec")) { Start-BitsTransfer -Source $down -Destination "$Temp\$exec" }
+	if (!(Test-Path -Path $exec)) { $exec = dw "https://www.technosys.net/download.aspx?file=syspin.exe" -ren "syspin.exe" }
 	New-Item -Path $gexe -ItemType File -Force | Out-Null
-	& "$Temp\$exec" "$gexe" $type | Out-Null | Out-Host
+	& $exec "$gexe" $type | Out-Null | Out-Host
 	do { Start-Sleep -Milliseconds 250 } until (Test-Path -Path $glnk)
 	Start-Sleep -Seconds 5
 	$shell = (New-Object -ComObject WScript.Shell).CreateShortcut($glnk)
@@ -154,19 +171,14 @@ function pin-to {
 function set-window {
 	param([int]$cpid = $PID)
 	$ErrorActionPreference = "SilentlyContinue"
-	$exec = "nircmd.exe"
-	$down = "https://www.nirsoft.net/utils/nircmd.zip"
-	$file = "nircmd.zip"
-	if (!(Test-Path -Path "$Temp\$exec")) {
-		Start-BitsTransfer -Source "$down" -Destination "$Temp\$file"
-		Expand-Archive -Path "$Temp\$file" -DestinationPath "$Temp" -Force
-	}
+	$exec = "$Temp\nircmd.exe"
+	if (!(Test-Path -Path $exec)) { dw "https://www.nirsoft.net/utils/nircmd.zip" }
 	$ppid = (Get-WmiObject Win32_Process -Filter "processid='$cpid'").ParentProcessId
 	$hwnd = (Get-Process -Id $ppid).MainWindowHandle
 	if (!$hwnd) { $hwnd = (Get-Process -Id $cpid).MainWindowHandle }
-	if (Test-Path -Path "$Temp\$exec") {
+	if (Test-Path -Path $exec) {
 		$sb = { param($exec, $hwnd); 'normal', 'activate', 'center' | ForEach-Object { & $exec win $_ handle $hwnd } }
-		Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath powershell.exe -ArgumentList "-command (Invoke-Command -ScriptBlock {$sb} -ArgumentList `"$Temp\$exec`", $hwnd)"
+		Start-Process -Verb RunAs -Wait -WindowStyle Hidden -FilePath powershell.exe -ArgumentList "-command (Invoke-Command -ScriptBlock {$sb} -ArgumentList `"$exec`", $hwnd)"
 	}
 }
 
