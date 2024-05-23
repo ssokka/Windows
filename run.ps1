32b48f01e46c5cf0cdbba79c5a5c0ddc67d2e6c1
ssokka <ssokka.root@gmail.com>
2024-05-23 18:08:41 +0900

    Add files via upload

---
+++ run.ps1
@@ -0,0 +1,28 @@
+﻿param(
+	[string]$pname,
+	[string]$tname,
+	[string]$sname,
+	[string]$fname,
+	[bool]$wait = $true
+)
+
+if (!(Get-Command -Name set-window -CommandType Function 2>$null)) { Invoke-Expression -Command ([Net.WebClient]::new()).DownloadString("https://raw.githubusercontent.com/ssokka/Windows/master/header.ps1") }
+
+try {
+	$host.ui.RawUI.WindowTitle = $tname
+	Write-Host "`n### $tname" -ForegroundColor Green
+	Write-Host "`n# $sname" -ForegroundColor Blue
+	$down = dw "$Git/$pname/$fname.7z" -wri $false
+	. "$Temp\fname.ps1"
+	
+	if ($wait) {
+		set-window
+		Write-Host "`n### 완료" -ForegroundColor Green
+		Write-Host "`n아무 키나 누르십시오..." -NoNewline; Read-Host
+	}
+}
+catch {
+	Write-Error ($_.Exception | Format-List -Force | Out-String) -ErrorAction Continue
+	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String) -ErrorAction Continue
+	throw
+}
