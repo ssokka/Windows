4160052c8e9bb9622208faf948e0f8b8a5b0233f
ssokka <ssokka.root@gmail.com>
2024-05-23 14:51:49 +0900

    Add files via upload

---
+++ Xshell/restore.ps1
@@ -0,0 +1,11 @@
+﻿if (!(Get-Command -Name set-window -CommandType Function 2>$null)) { Invoke-Expression -Command ([Net.WebClient]::new()).DownloadString("https://raw.githubusercontent.com/ssokka/Windows/master/header.ps1") }
+
+try {
+	$down = dw "$Git/$name/restore.7z" -wri $false
+	Start-Process -Verb RunAs -Wait -FilePath wt.exe -ArgumentList "powershell.exe", "$Temp\restore.ps1"
+}
+catch {
+	Write-Error ($_.Exception | Format-List -Force | Out-String) -ErrorAction Continue
+	Write-Error ($_.InvocationInfo | Format-List -Force | Out-String) -ErrorAction Continue
+	throw
+}
